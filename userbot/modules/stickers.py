a56a2fb0b2c59e2bf7344671bcaf5d87adb10d1b
afdulfauzan <afdulfauzan.id@gmail.com>
2020-05-05 20:51:17 +0700

    repair the name of the sticker pack

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -87,7 +87,7 @@ async def kang(args):
                 emoji = splat[1]
 
         packname = f"{user.username}_{pack}"
-        packnick = f"{name}"
+        packnick = f"{user.first_name}"
         cmd = '/newpack'
         file = io.BytesIO()
 
@@ -115,7 +115,7 @@ async def kang(args):
                 while "120" in x.text:
                     pack += 1
                     packname = f"{user.username}_{pack}"
-                    packnick = f"{name}"
+                    packnick = f"{user.first_name}"
                     await args.edit("`Switching to Pack " + str(pack) +
                                     " due to insufficient space`")
                     await conv.send_message(packname)
